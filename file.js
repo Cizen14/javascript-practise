0f3936230c2670d11f02186c0f766dbc91ba1589
Cizen <cizenmah@gmail.com>
2023-11-28 18:12:22 +1100

    javascript-practise

---
+++ file.js
@@ -0,0 +1,93 @@
+// console.log('Hello from JavaScript file');
+// console.log('Hello from JavaScript file');
+// console.log('Hello from JavaScript file');
+// console.log('Hello from JavaScript file');
+
+//variable first
+
+var firstName= 'Cizen'
+let address;
+address ='Rockdale'
+console.log('this is my address', address);
+
+const phone = 474305412;
+console.log('this is my phone' + phone)
+
+//data types 
+
+const  subject ='physics';
+let isStudying ='true';
+// let hobbies= 'Undefined';
+
+// reference types
+
+//Arrays
+const numbers= [10, 0 ,20]
+const hobbies= [ 'trekking', 'climbing','running']
+
+//objects 
+
+var student = {
+    id: 123,
+    
+    dob: new Date('12-12-2000'),
+    address : 'rootyhill',
+    isEnrolled : false
+}
+console.log('this is the sudent information', student);
+console.log ( 'this is the hobby', hobbies[0])
+
+//functions
+function printMyName(){
+    const salay = 30;
+    const hours = 5;
+    const amount = salay * hours;
+    console.log('I earned' , amount);
+
+ 
+}
+printMyName();
+
+
+//EcmaScript
+//ES6
+
+
+
+const printFavouriteFood =() => {
+    let type = 'Non-veg';
+
+
+    switch(type){
+        case'vegeterian':
+        console.log('Paneer')
+        break;
+        case 'Non-veg':
+        console.log('Masu')
+        break;
+        case 'vegan':
+        console.log('plant')
+        break;
+        default:
+            console.log('no food available')
+
+    }
+}
+printFavouriteFood();
+
+
+
+//error handling
+
+try{
+   const response= fetch('https://app.chaano.com/api/v1');
+
+}
+catch(error){
+{
+    console.log(error.message)
+    alert('Something went wrong!', error.message)
+}
+}finally{
+
+}
