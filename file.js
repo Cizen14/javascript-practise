4df39bb8d445dd50a4ee533679ec1079988284b7
Cizen <cizenmah@gmail.com>
2023-11-28 21:22:30 +1100

    seccond commit

--- file.js
+++ file.js
@@ -1,93 +1,108 @@
-// console.log('Hello from JavaScript file');
-// console.log('Hello from JavaScript file');
-// console.log('Hello from JavaScript file');
-// console.log('Hello from JavaScript file');
+// // console.log('Hello from JavaScript file');
+// // console.log('Hello from JavaScript file');
+// // console.log('Hello from JavaScript file');
+// // console.log('Hello from JavaScript file');
 
-//variable first
+// //variable first
 
-var firstName= 'Cizen'
-let address;
-address ='Rockdale'
-console.log('this is my address', address);
+// var firstName= 'Cizen'
+// let address;
+// address ='Rockdale'
+// console.log('this is my address', address);
 
-const phone = 474305412;
-console.log('this is my phone' + phone)
+// const phone = 474305412;
+// console.log('this is my phone' + phone)
 
-//data types 
+// //data types 
 
-const  subject ='physics';
-let isStudying ='true';
-// let hobbies= 'Undefined';
+// const  subject ='physics';
+// let isStudying ='true';
+// // let hobbies= 'Undefined';
 
-// reference types
+// // reference types
 
-//Arrays
-const numbers= [10, 0 ,20]
-const hobbies= [ 'trekking', 'climbing','running']
+// //Arrays
+// const numbers= [10, 0 ,20]
+// const hobbies= [ 'trekking', 'climbing','running']
 
-//objects 
+// //objects 
 
-var student = {
-    id: 123,
+// var student = {
+//     id: 123,
     
-    dob: new Date('12-12-2000'),
-    address : 'rootyhill',
-    isEnrolled : false
-}
-console.log('this is the sudent information', student);
-console.log ( 'this is the hobby', hobbies[0])
+//     dob: new Date('12-12-2000'),
+//     address : 'rootyhill',
+//     isEnrolled : false
+// }
+// console.log('this is the sudent information', student);
+// console.log ( 'this is the hobby', hobbies[0])
 
-//functions
-function printMyName(){
-    const salay = 30;
-    const hours = 5;
-    const amount = salay * hours;
-    console.log('I earned' , amount);
+// //functions
+// function printMyName(){
+//     const salay = 30;
+//     const hours = 5;
+//     const amount = salay * hours;
+//     console.log('I earned' , amount);
 
  
-}
-printMyName();
+// }
+// printMyName();
 
 
-//EcmaScript
-//ES6
-
-
-
-const printFavouriteFood =() => {
-    let type = 'Non-veg';
-
-
-    switch(type){
-        case'vegeterian':
-        console.log('Paneer')
-        break;
-        case 'Non-veg':
-        console.log('Masu')
-        break;
-        case 'vegan':
-        console.log('plant')
-        break;
-        default:
-            console.log('no food available')
-
-    }
-}
-printFavouriteFood();
+// //EcmaScript
+// //ES6
 
 
 
-//error handling
+// const printFavouriteFood =() => {
+//     let type = 'Non-veg';
 
-try{
-   const response= fetch('https://app.chaano.com/api/v1');
 
-}
-catch(error){
-{
-    console.log(error.message)
-    alert('Something went wrong!', error.message)
-}
-}finally{
+//     switch(type){
+//         case'vegeterian':
+//         console.log('Paneer')
+//         break;
+//         case 'Non-veg':
+//         console.log('Masu')
+//         break;
+//         case 'vegan':
+//         console.log('plant')
+//         break;
+//         default:
+//             console.log('no food available')
 
-}
+//     }
+// }
+// printFavouriteFood();
+
+
+
+// //error handling
+
+// try{
+//    const response= fetch('https://app.chaano.com/api/v1');
+
+// }
+// catch(error){
+// {
+//     console.log(error.message)
+//     alert('Something went wrong!', error.message)
+// }
+// }finally{
+// console.log("it will always run")
+// }
+
+
+//es6
+//template literals
+
+// const name ="cizen"
+// const add ="sydney"
+// const info = ` Hi my name is ${name}. I live in ${add}.`
+
+// console.log(info)
+
+//arrow function 
+// create call , invoke
+
+// if you dont put {} you dont need to write return. but you do you need to write the written.
