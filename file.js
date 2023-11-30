ef1ee4a0c59df6efa3186627f82dfd431b03ebd2
Cizen <cizenmah@gmail.com>
2023-11-30 21:10:53 +1100

    next commit

--- file.js
+++ file.js
@@ -118,3 +118,90 @@
 
 const fruits =[ 'apples', 'mapples', 'banana']
 console.log(fruits);
+console.log(fruits[1]); // access
+console.log(fruits.length); // check the length.
+
+fruits[3]='pineapple'; // this element is for adding new element.
+//push will add the element in the last.
+fruits.push('cherry');
+
+
+//to remove the element it removes the last element no need to put the value.
+
+fruits.pop();
+console.log(fruits);
+
+// to remove the element where first param is the one where the delete number starts and the scond param means the number of elements to delete.
+//we can add the element in the splice as well where you need to put the name in the 3rd param
+
+fruits.splice(0,2);
+console.log(fruits);
+
+//to remove first element
+
+fruits.shift();
+console.log(fruits);
+fruits.unshift('apple') // to add the first elmenet
+console.log(fruits);
+
+
+//IndexOf to know the index number 
+
+console.log(fruits.indexOf('kiwi'))
+console.log(fruits);
+
+                            // mutation array means the orginal array is getting updated evertime.
+// concat
+// const veges = ['tomato', 'onion', 'garlic', 'potato'];
+// const vegesandfruits = fruits.concat(veges);
+// console.log(fruits, veges, vegesandfruits);
+
+
+// how to remove the mutating the original array.
+console.log(fruits);
+console.log(...fruits);
+const newFruits = [...fruits];
+newFruits.push("berries");
+console.log(newFruits);
+
+console.log(fruits);
+
+//writing a loop for fruits to make it all uppercase
+
+
+
+let uppercaseFruits=[];
+for(let i=0; i<fruits.length; i++){
+    console.log(fruits[i].toUpperCase());
+    uppercaseFruits.push(
+        fruits[i].toUpperCase()
+    )
+}
+console.log(uppercaseFruits);
+
+
+fruits.forEach(i=>{
+    console.log(i);
+})
+
+// map
+
+const mappedFruits = fruits.map(fruit=>
+    fruit.toUpperCase()
+    
+)
+console.log(mappedFruits);
+
+
+//includes it checks whether it has in the element or not
+// console.log(fruits.includes('apple' || 'mango'));
+
+
+//every 
+
+console.log(fruits.every(element => fruits.length > 2 ));
+
+//some
+console.log(fruits.some(element => fruits.includes('a') ));
+
+
